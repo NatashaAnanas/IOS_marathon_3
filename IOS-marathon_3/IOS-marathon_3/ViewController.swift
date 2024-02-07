72d8d9f01bde350fc7c78f46fceb1a995669c642
NatashaAnanas <konnat7@mail.ru>
2024-02-07 13:45:51 +0300

    Base settings

---
+++ IOS-marathon_3/IOS-marathon_3/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  IOS-marathon_3
+//
+//  Created by Наталья Коновалова on 07.02.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
